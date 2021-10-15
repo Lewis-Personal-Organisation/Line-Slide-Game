839aa6d3483e1c4479838d1090718671f852ed62
Lewis Dawson <lewis-dawson@live.co.uk>
2021-10-15 17:25:46 +0100

    UI Update
    - Added Tap to Play Text
    - Added functionality for lerping scale of text

---
+++ LineGame/Assets/Scripts/Utility/UI/Text/TextBounce.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TextBounce : MonoBehaviour
+{
+    public RectTransform textTransform;
+    private Vector3 defaultScale = Vector3.zero;
+
+    public float speed = 2;
+    public float scaleMin;
+    public float scaleMax;
+
+    private bool isGrowing = false;
+
+    private bool isEnabled;
+
+
+    private void Awake()
+    {
+        defaultScale = textTransform.localScale;
+    }
+
+	private void OnEnable()
+	{
+        Bounce();
+	}
+
+    private void Update()
+    {
+        if (!isEnabled)
+            return;
+
+        if (isGrowing)
+        {
+            if (textTransform.localScale.x < scaleMax)
+            {
+                textTransform.localScale += new Vector3(Time.deltaTime, Time.deltaTime, 0) * speed;
+            }
+            else
+            {
+                textTransform.localScale = new Vector3(scaleMax, scaleMax, 0);
+                isGrowing = false;
+            }
+        }
+        else
+        {
+            if (textTransform.localScale.x >= scaleMin)
+            {
+                textTransform.localScale -= new Vector3(Time.deltaTime, Time.deltaTime, 0) * speed;
+            }
+            else
+            {
+                isGrowing = true;
+                textTransform.localScale = new Vector3(scaleMin, scaleMin, 0);
+            }
+        }
+    }
+
+	public void Stop()
+	{
+        isEnabled = false;
+	}
+
+    public void Bounce()
+	{
+        isEnabled = true;
+	}
+}
